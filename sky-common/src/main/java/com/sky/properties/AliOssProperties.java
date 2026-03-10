2c336b8bd9b5a02ad1629a7244c82a35fd5c2d03
AmeRaincandy <1943260721@qq.com>
2026-03-10 22:07:30 +0800

    完善了新增菜品业务的代码

--- sky-common/src/main/java/com/sky/properties/AliOssProperties.java
+++ sky-common/src/main/java/com/sky/properties/AliOssProperties.java
@@ -4,14 +4,13 @@ import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
-@Component
-@ConfigurationProperties(prefix = "sky.alioss")
 @Data
+@Component
+//@ConfigurationProperties注解 用于将配置文件中的属性值注入到该类中
+//prefix属性指定了配置文件中属性的前缀，只有以该前缀开头的属性才会被注入
+@ConfigurationProperties(prefix = "aliyun.oss")
 public class AliOssProperties {
-
     private String endpoint;
-    private String accessKeyId;
-    private String accessKeySecret;
     private String bucketName;
-
+    private String region;
 }
---
+++ sky-server/src/main/java/com/sky/controller/admin/CommonController.java
@@ -0,0 +1,42 @@
+package com.sky.controller.admin;
+
+import com.sky.constant.MessageConstant;
+import com.sky.properties.AliOssProperties;
+import com.sky.result.Result;
+import com.sky.utils.AliOssUtil;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+/*
+* 通用接口
+* */
+@Slf4j
+@Api(tags = "通用接口")
+@RestController
+@RequestMapping("/admin/common")
+public class CommonController {
+
+    @Autowired
+    private AliOssUtil aliOssUtil;
+
+    @PostMapping("/upload")
+    @ApiOperation("文件上传")
+    public Result<String> upload(MultipartFile file) {
+        log.info("文件上传：{}", file);
+
+        try {
+            String url = aliOssUtil.upload(file.getBytes(), file.getOriginalFilename());
+            log.info("文件上传成功，url：{}", url);
+            return Result.success(url);
+        } catch (Exception e) {
+            log.info("文件上传失败：{}", e.getMessage());
+        }
+        return Result.error(MessageConstant.UPLOAD_FAILED);
+    }
+}
